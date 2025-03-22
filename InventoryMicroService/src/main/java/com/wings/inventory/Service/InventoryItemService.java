2978803d5120e3293037804f2ae24b44c0bc729e
Bhanaka <bhanaka94pramod@gmail.com>
2025-03-22 23:20:16 +0530

    create anew service class

--- InventoryMicroService/src/main/java/com/wings/inventory/Service/InventoryItemService.java
+++ InventoryMicroService/src/main/java/com/wings/inventory/Service/InventoryItemService.java
@@ -2,6 +2,7 @@ package com.wings.inventory.Service;
 
 import com.wings.inventory.Dto.InventoryItemDTO;
 import com.wings.inventory.Model.InventoryItemEntity;
+import com.wings.inventory.Repository.InventoryItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,16 +11,31 @@ import org.springframework.transaction.annotation.Transactional;
 //@Transactional
 public class InventoryItemService {
 //    @Autowired private  InventoryItemDTO itemDTO ;
+    @Autowired
+    private InventoryItemRepository inventoryItemRepository ;
     public InventoryItemDTO createInventoryItem(InventoryItemDTO inventoryItemDTO){
+
         InventoryItemEntity inventoryItemEntity =new InventoryItemEntity() ;
         // Mapping fields from DTO to Entity
+        inventoryItemEntity.setItemCode(inventoryItemDTO.getItemCode());
+        inventoryItemEntity.setItemName(inventoryItemDTO.getItemName());
+        inventoryItemEntity.setItemBrand(inventoryItemDTO.getItemBrand());
+        inventoryItemEntity.setItemBuyPrice(inventoryItemDTO.getItemBuyPrice());
+        inventoryItemEntity.setItemSellingPrice(inventoryItemDTO.getItemSellingPrice());
+        inventoryItemEntity.setItemIsActive(inventoryItemDTO.getItemIsActive());
 
-//        inventoryItemEntity.setItemCode(inventoryItemDTO.getItemCode());
-//        inventoryItemEntity.setItemName(inventoryItemDTO.getItemName());
-//        inventoryItemEntity.setItemBrand(inventoryItemDTO.getItemBrand());
-//        inventoryItemEntity.setItemBuyPrice(inventoryItemDTO.getItemBuyPrice());
-//        inventoryItemEntity.setItemSellingPrice(inventoryItemDTO.getItemSellingPrice());
-//        inventoryItemEntity.setItemIsActive(inventoryItemDTO.getItemIsActive());
+        // save the entity to the database
+        InventoryItemEntity savedEntity = inventoryItemRepository.save(inventoryItemEntity) ;
+
+        // Convert the saved entity back to a DTO to return
+        InventoryItemDTO savedItemDTO = new InventoryItemDTO();
+                savedEntity.getItemCode();
+                savedEntity.getItemName();
+                savedEntity.getItemBrand();
+                savedEntity.getItemBuyPrice();
+                savedEntity.getItemSellingPrice();
+                savedEntity.getItemIsActive();
+
         return inventoryItemDTO;
 
     }
