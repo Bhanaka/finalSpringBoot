df6a5b9b1d79dcf18d92b6bdb30a153a7fb02476
Pramod Bhanaka <bhanaka94pramod@gmail.com>
2025-05-07 15:18:40 +0530

    create new ignore file

--- ProductMicroService/src/main/java/com/wings/product/Controllers/ProductController.java
+++ ProductMicroService/src/main/java/com/wings/product/Controllers/ProductController.java
@@ -16,15 +16,15 @@ public class ProductController {
     @Autowired
     private ProductBrandService productBrandService ;
     @PostMapping("/create_brand")
-    public ResponseEntity<ProductBrandEntity> createBrand(@RequestBody ProductBrandDTO productBrandDTO){
-//        ProductBrandEntity brand = new ProductBrandEntity();
-//        brand.setDescription(productBrandDTO.getDescription());
-//        brand.setIsActive(productBrandDTO.getIsActive());
-//        productBrandService
-//        System.out.println("new brands");
-        return  productBrandService.createProductBrand(productBrandDTO);
-//        return "create new brands" ;
-    }
+//    public ResponseEntity<ProductBrandEntity> createBrand(@RequestBody ProductBrandDTO productBrandDTO){
+////        ProductBrandEntity brand = new ProductBrandEntity();
+////        brand.setDescription(productBrandDTO.getDescription());
+////        brand.setIsActive(productBrandDTO.getIsActive());
+////        productBrandService
+////        System.out.println("new brands");
+//        return  productBrandService.createProductBrand(productBrandDTO);
+////        return "create new brands" ;
+//    }
     @GetMapping("/get_all_brands")
     public List<ProductBrandEntity> getAllBrands(){
         List brandList =productBrandService.getAllProductBrand() ;
